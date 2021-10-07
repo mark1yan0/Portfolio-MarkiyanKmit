ecf22350269485602f6ef3861c67a9b5625d0e38
Mark1yan1 <markkmit@gmail.com>
2021-10-08 01:20:18 +0200

    updates

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,90 +1,96 @@
 'use strict';
-/* ============
-    Project cards
+/* Project cards
    ============ */
 
 // projects data
-let projects = {
-  images: {
-    image: [
-      './assets/media/prynce.png',
-      './assets/media/air-quality.png',
-      './assets/media/react-books.png',
-    ],
-    alt: [
-      'Prynce Original Lab',
-      `Progetto Qualità dell'aria`,
-      'Progetto libreria',
-    ],
+let projects = [
+  //prynce
+  {
+    image: './assets/media/prynce.png',
+    alt: 'Prynce Original Lab',
+    links: {
+      githubLink: '',
+      projectLink: 'https://www.prynce.it/',
+      linkTitle: 'www.prynce.it',
+    },
+    title: 'Prynce Original Lab',
+    description: {
+      p1: 'Progetto di e-commerce al quale lavoro da novembre 2020 sfruttando WordPress, WooCommerce, Elementor, altri plug-in e CSS personalizzato.',
+      p2: 'Questo progetto mi ha permesso di capire importanti concetti sulla costruzione di siti web e la sua gestione mi permette continuamente di migliorarmi ed aggiornarmi.',
+      p3: '',
+    },
   },
-  links: {
-    githubLinks: [
-      '',
-      'https://github.com/mark1yan0/s2i_airQuality',
-      'https://github.com/mark1yan0/react-project-s2i',
-    ],
-    projectLinks: [
-      'https://www.prynce.it/',
-      'https://nervous-bhabha-b42b4b.netlify.app/',
-      'https://react-books-321518.web.app/',
-    ],
-    linkTitle: ['www.prynce.it', 'AirQuality Project', "A Book's Good Project"],
+
+  //progetto js advanced
+  {
+    image: './assets/media/air-quality.png',
+    alt: "Progetto Qualità dell'aria",
+    links: {
+      githubLink: 'https://github.com/mark1yan0/s2i_airQuality',
+      projectLink: 'https://nervous-bhabha-b42b4b.netlify.app/',
+      linkTitle: 'AirQuality Project',
+    },
+    title: 'Air Quality',
+    description: {
+      p1: "Questo progetto mostra l'indice di qualità dell'aria di una zona in base alla geolocalizzazione dell'utente e alla ricerca della città.",
+      p2: 'Grazie a questo progetto ho imparato a lavorare con richieste di API, dati in JSON, costruzione di bundle in webpack e in generale scrivere codice JavaScript più avanzato.',
+      p3: 'La mappa è stata implementata grazie alla libreria LeafletJS, tutto il resto è stato scritto in vanilla JavaScript.',
+    },
   },
-  titles: ['Prynce Original Lab', 'Air Quality', "A Book's Good"],
-  descriptions: {
-    p1: [
-      'Progetto di e-commerce al quale lavoro da novembre 2020 sfruttando WordPress, WooCommerce, Elementor, altri plug-in e CSS personalizzato',
-      "Questo progetto mostra l'indice di qualità dell'aria di una zona in base alla geolocalizzazione dell'utente e alla ricerca della città",
-      'Questo è un progetto reatizzato con la libreria ReactJs con la Google Books API e tecnologie come React Router, Axios, Context API e altre.',
-    ],
-    p2: [
-      'Questo progetto mi ha permesso di capire importanti concetti sulla costruzione di siti web e la sua gestione mi permette continuamente di migliorarmi ed aggiornarmi.',
-      'Grazie a questo progetto ho imparato a lavorare con richieste di API, dati in JSON, costruzione di bundle in webpack e in generale scrivere codice JavaScript più avanzato',
-      "Questo progetto mi ha permesso di addentrarmi dentro uno state managment un po' più complesso del solito, infatti presenta la funzionalità, oltre al poter cercare libri, di aggiungerli ad una lista di preferiti che viene salvata in local storage",
-    ],
-    p3: [
-      '',
-      'La mappa è stata implementata grazie alla libreria LeafletJS, tutto il resto è stato scritto in vanilla JavaScript',
-      'Maggiori dettagli nel file README su GitHub.',
-    ],
+
+  //progetto react
+  {
+    image: './assets/media/react-books.png',
+    alt: 'Progetto libreria',
+    links: {
+      githubLink: 'https://github.com/mark1yan0/react-project-s2i',
+      projectLink: 'https://react-books-321518.web.app/',
+      linkTitle: "A Book's Good Project",
+    },
+    title: "A Book's Good",
+    description: {
+      p1: 'Questo è un progetto reatizzato con la libreria ReactJs con la Google Books API e tecnologie come React Router, Axios, Context API e altre.',
+      p2: "Questo progetto mi ha permesso di addentrarmi dentro uno state managment un po' più complesso del solito, infatti presenta la funzionalità, oltre al poter cercare libri, di aggiungerli ad una lista di preferiti che viene salvata in local storage.",
+      p3: '',
+    },
   },
-};
+];
 
 //creating  project cards
 function createProjects() {
   let projectCard = document.querySelectorAll('.project-card'); //Need to insert divs in html
-  for (let i = 0; i < projects.titles.length; i++) {
+  for (let i = 0; i < projects.length; i++) {
     projectCard[i].innerHTML = `
         <div class="project-image">
             <!-- photo ratio 2:1 -->
             <img
-                src="${projects.images.image[i]}"
-                alt="${projects.images.alt[i]}"/>
+                src="${projects[i].image}"
+                alt="${projects[i].alt}"/>
             <div class="project-links">
                 <a class="git-icon ${
-                  projects.links.githubLinks[i] === '' ? 'hide' : ''
+                  projects[i].links.githubLink === '' ? 'hide' : ''
                 }" 
-                    href="${projects.links.githubLinks[i]}" target="_blank">
+                    href="${projects[i].links.githubLink}" target="_blank">
                     <i class="fab fa-github" title="GitHub"></i>
                 </a>
-                <a href="${projects.links.projectLinks[i]}" target="_blank">
+                <a href="${projects[i].links.projectLink}" target="_blank">
                     <i class="fas fa-external-link-alt"
-                        title="${projects.links.linkTitle[i]}">
+                        title="${projects[i].links.linkTitle}">
                     </i>
                 </a>
             </div>
         </div>
         <div class="project-description">
-            <h3>${projects.titles[i]}</h3>
+            <h3>${projects[i].title}</h3>
             <hr />
             <p>
-                ${projects.descriptions.p1[i]}
+                ${projects[i].description.p1}
             </p>
             <p>
-                ${projects.descriptions.p2[i]}
+                ${projects[i].description.p2}
             </p>
             <p>
-                ${projects.descriptions.p3[i]}
+                ${projects[i].description.p3}
             </p>
         </div>`;
   }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,15 @@
 'use strict';
-/* ============
-    Nav menu
+/*  Nav menu
    ============ */
-// //variables
-// let nav = document.getElementById('nav');
 
-// //functions
-// function openNav() {
-//   nav.style.cssText = `@media only screen and (max-width: 992px) {
-//     width: 50px;
-//     height: 50px;
-//     overflow: hidden;
-//     margin: 10px;
-//     border
-//     border-radius: 50%;
-//   }`;
-// }
+let nav = document.getElementById('nav');
+let navBtnOpen = document.querySelector('.open');
+let navBtnClose = document.querySelector('.close');
+let navItem = document.querySelector('.nav-list li');
+let navOpen = false;
 
-// openNav();
-
-// //aventListeners
-
-// /* ============
-//     Tools popup
-//    ============ */
-// //variables
-// let tools = document.querySelectorAll('.tool');
-// let expPopup = document.getElementById('exp');
-// let closePopup = document.querySelector('.btn-close');
-// let overlay = document.createElement('div');
-
-// //functions
-// function showPopup() {
-//   expPopup.style.width = `40%`;
-
-//   overlay.style.cssText = `position: fixed;
-//   width: 100%;
-//   height: 100%;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   background-color: rgba(0,0,0,0.5);
-//   z-index: 2;
-//   cursor: pointer;`;
-//   document.body.prepend(overlay);
-// }
-
-// function hidePopup() {
-//   expPopup.style.width = `0`;
-//   document.body.style.backgroundColor = '';
-//   overlay.style.cssText = '';
-//   overlay.remove;
-// }
-
-// //eventListeners
-// for (let i = 0; i < tools.length; i++) {
-//   tools[i].addEventListener('click', showPopup);
-// }
-
-// closePopup.addEventListener('click', hidePopup);
-// overlay.addEventListener('click', hidePopup);
+navBtnOpen.addEventListener('click', () => nav.classList.add('nav__active'));
+navBtnClose.addEventListener('click', () =>
+  nav.classList.remove('nav__active')
+);
+navItem.addEventListener('click', () => nav.classList.remove('nav__active'));
